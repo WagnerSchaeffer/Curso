52d128ab09f967a39c575158acf7a13e60bd1e27
wagner <wagner@wagner-P430-G-BC45P1>
2019-05-08 00:39:22 -0300

    Criado pacote cap5 e class teste

---
+++ CursoJava/src/cap5/aulaTeste.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cap5;
+
+/**
+ * @author wagner
+ *
+ */
+public class aulaTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Aula teste");
+
+	}
+
+}
